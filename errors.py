61143527a7af241631625a57703bcf6c64bf7661
Julian Sim <juliansim1729@gmail.com>
2020-06-14 13:05:25 -0400

    Adding custom errors.

--- errors.py
+++ errors.py
@@ -1,7 +1,7 @@
-class StockNotFound(Exception):
+class StockNotFoundError(Exception):
     """Raised when the desired stock is not found within the currently recognized stocks in the system."""
     pass
 
-class NotEnoughLiquidCash(Exception):
+class NotEnoughLiquidCashError(Exception):
     """Raised when the player does not have enough liquid cash to buy the stocks or to pay the required taxes."""
     pass
